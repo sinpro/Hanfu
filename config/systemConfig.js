4c683d415e9af15278a36a72e0c0dfe1280b5e3c
豆江涛 <doujiangtao@doujiangtaodeMacBook-Pro.local>
2021-01-30 22:25:17 +0800

    hh

--- config/systemConfig.js
+++ config/systemConfig.js
@@ -1,42 +1,18 @@
-<<<<<<< HEAD
-// dev
-// fat
-// uat
-// prd
-
-var obj = {
-    development: {
-        baseurl: 'http://www.baidu.com/indexejs'
+module.exports = {
+    // dev
+    dev_CONFIG: {
+        baseUrl: 'http://www.baidu.com/dev'
     },
-    fat: {
-        baseurl: 'http://www.baidu.com/indexejs'
+    // fat
+    fat_CONFIG: {
+        baseUrl: 'http://www.baidu.com/fat'
     },
-    uat: {
-        baseurl: 'http://www.baidu.com/indexejs'
+    // uat
+    uat_CONFIG: {
+        baseUrl: 'http://www.baidu.com/uat'
     },
-    prd: {
-        baseurl: 'http://www.baidu.com/indexejs'
+    // prd
+    prd_CONFIG: {
+        baseUrl: 'http://www.baidu.com/prd'
     }
 }
-
-export default obj;
-=======
-module.exports = {
-	// dev
-	dev_CONFIG: {
-		baseUrl: 'http://www.baidu.com/dev'
-	},
-	// fat
-	fat_CONFIG: {
-		baseUrl: 'http://www.baidu.com/fat'
-	},
-	// uat
-	uat_CONFIG: {
-		baseUrl: 'http://www.baidu.com/uat'
-	},
-	// prd
-	prd_CONFIG: {
-		baseUrl: 'http://www.baidu.com/prd'
-	}
-}
->>>>>>> 3551c326535700e48a21f61e309146a16bc00906
